072bf879dd5e24be0e096f4e1f015321ceecef10
Mouad Louhibi <mouadlouhibi2000@gmail.com>
2022-04-23 14:56:26 +0000

    Auth Controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,4 +7,13 @@ use Illuminate\Http\Request;
 class AuthController extends Controller
 {
     //
+    /**
+     * Create a new AuthController instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login']]);
+    }
 }
