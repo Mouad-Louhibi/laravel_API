03c4ef35bb69db0abc416b152e1eabadaa602c59
Mouad Louhibi <mouadlouhibi2000@gmail.com>
2022-04-25 22:31:38 +0000

    Auth Controller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -65,7 +65,7 @@ class AuthController extends Controller
      */
     public function refresh()
     {
-        return $this->respondWithToken(auth('api')->refresh());
+        return $this->respondWithToken(JWTAuth::refresh());
     }
 
     /**
